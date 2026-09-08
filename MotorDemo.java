f79fb98370beea0403982d650722a7e6c605702e
clevymargareth <clevybria236@gmail.com>
2026-09-08 08:35:19 +0700

    Add Experiment 2: Encapsulation and Setters/Getters

--- MotorDemo.java
+++ MotorDemo.java
@@ -1,39 +1,20 @@
 public class MotorDemo {
     public static void main(String[] args) {
-        
         Motor motor1 = new Motor();
         motor1.displayStatus();
-        motor1.platNomor = "B 0838 XZ";
-        int kecepatanBaru1 = 50;
-        
-        if (!motor1.isMesinOn && kecepatanBaru1 > 0) {
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        } else {
-            motor1.kecepatan = kecepatanBaru1;
-        }
+        motor1.setPlatNomor("B 0838 XZ");
+        motor1.setKecepatan(50);
         motor1.displayStatus();
 
         Motor motor2 = new Motor();
-        motor2.platNomor = "N 9840 AB";
-        motor2.isMesinOn = true;
-        int kecepatanBaru2 = 40;
-        
-        if (!motor2.isMesinOn && kecepatanBaru2 > 0) {
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        } else {
-            motor2.kecepatan = kecepatanBaru2;
-        }
+        motor2.setPlatNomor("N 9840 AB");
+        motor2.setIsMesinOn(true);
+        motor2.setKecepatan(40);
         motor2.displayStatus();
 
         Motor motor3 = new Motor();
-        motor3.platNomor = "D 8343 CV";
-        int kecepatanBaru3 = 60;
-        
-        if (!motor3.isMesinOn && kecepatanBaru3 > 0) {
-            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
-        } else {
-            motor3.kecepatan = kecepatanBaru3;
-        }
+        motor3.setPlatNomor("D 8343 CV");
+        motor3.setKecepatan(60);
         motor3.displayStatus();
     }
 }
